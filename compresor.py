ba074c9917fa410974e90c26bc68f740dd7bf19a
Patricia <pribesme7@alumnes.ub.edu>
2016-10-09 18:42:43 +0200

    [Huffman compresor] Huffman compresor completed!

--- compresor.py
+++ compresor.py
@@ -1,17 +1,17 @@
 #*******************************************************************************
 #                                                                              *
-#    program: Compresor                                                        *
+#    program: Huffman Compresor                                                *
 #    author : Patricia Ribes Metidieri                                         *
 #                                                                              *
 #*******************************************************************************
 
 
-"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    LIBRARIES     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    LIBRARIES     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 import numpy as np
 import collections
-
-
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    FUNCTIONS     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+import time
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    CLASSES      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 class huffman_node(object):
     def __init__(self, value, key =  None,  left=None, right = None):
         self.left = left
@@ -21,6 +21,9 @@ class huffman_node(object):
 
     def children(self):
         return ((self.left, self.right))
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    FUNCTIONS     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 def frequencies(text):
     freq = collections.OrderedDict()
@@ -33,63 +36,68 @@ def frequencies(text):
     freq = sorted(freq.items(), key=lambda t: t[1])
     return freq
 
-def walk_huffman_tree(node,num_elements):
-    codes = {}
-    code = []
-    childrens = ()
-    while len(codes) < num_elements:
+def build_huffman_tree(freq):
+    forest = []
+    for key, value in freq:
+
+        forest.append(huffman_node(value, key))
+
+    while len(forest) > 1:
+        left = forest.pop(0)
+        right = forest.pop(0)
+        forest.append(huffman_node(left.value + right.value, left=left, right=right))
+        forest.sort(key=lambda x: x.value)
+
+    huffman_tree = forest[0]
+    return  huffman_tree
+
+
+def get_path(node,path=[],codes={}):
+    if node.key == None:
         for i in range(2):
-            if node.children()[i].key != None:
+            path.append("%i"%(int(not i)))
+            get_path(node.children()[i],path,codes)
+            path.pop()
 
+    elif node.key not in codes.keys():
+        codes[node.key] = "".join(path)
+        return get_path(node, path, codes)
 
-            code[i] = string(code[i]) + string(i)
-            walk_huffman_tree(node.children()[i],num_elements-1)
+    return codes
 
+def huffman_codification(text):
+    freq = frequencies(text)
+    huffman_tree = build_huffman_tree(freq)
+    codes = get_path(huffman_tree)
+    return codes
 
+def write_compressed_file(codes,text):
+    comp_file = open("quijote_huffman.txt","w")
+    comp_file.write("{codes:{")
+    for key,value in codes.items():
+        comp_file.write(key)
+        comp_file.write(":")
+        comp_file.write(value)
+        comp_file.write(",")
+    comp_file.write("},{text:")
 
+    for char in text:
+        comp_file.write(codes[char])
+    comp_file.write("}")
+    comp_file.close()
 
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    MAIN    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+def main():
+    start_time = time.time()
+    text = []
+    with open("prueba.txt") as file:
+        lines = list(file.read())
+        text.extend(lines)
 
+    codes = huffman_codification(text)
+    write_compressed_file(codes,text)
+    print "Time =", time.time() - start_time, "s"
 
-
-
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    MAINN    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-dict= collections.OrderedDict()
-dict["o"] = 3
-dict["b"] = 1
-dict["p"] = 1
-dict["'"] = 2
-dict["m"] = 2
-dict["j"] = 3
-dict[" "] = 12
-dict["d"] = 3
-dict["i"] = 4
-dict["r"] = 5
-dict["u"] = 5
-dict["a"] = 4
-dict["l"] = 6
-dict["s"] = 6
-dict["e"] = 8
-
-ex =  collections.OrderedDict()
-
-ex["1"] = 0.25
-ex["2"] = 0.25
-ex["3"] = 0.20
-ex["4"] = 0.15
-ex["5"] = 0.15
-ex = sorted(ex.items(), key=lambda t: t[1])
-print ex
-forest = []
-for key,value in ex:
-    print key,value
-    forest.append(huffman_node(value,key))
-
-print len(forest)
-
-while len(forest)>1:
-    left = forest.pop(0)
-    right = forest.pop(0)
-    forest.append(huffman_node(left.value+right.value,left = left, right = right))
-    forest.sort( key=lambda x: x.value)
-
+if __name__ == "__main__":
+    main()
 
